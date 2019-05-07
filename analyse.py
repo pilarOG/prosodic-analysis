043b0ea8c5207d05787082a379c6cc2942397dbd
pilarOG <psoplust@uc.cl>
2019-05-07 14:38:38 +0100

    initial commit

---
+++ analyse.py
@@ -0,0 +1,16 @@
+# Features to extract
+# 1) F0: mean, range, standard deviation, contour
+# 2) Intensity: mean, range, standard deviation, contour, mean c0 coefficient
+# 3) Duration: range, mean (without silences), standard deviation
+# 4) Speech rate: units per second
+# 5) Vocal quality: HNR, glottal source open quotient
+# 6) Pause: mean duration, range duration, pauses per second, ratio of pauses/speech
+
+
+# Two modes:
+# 1) Collect statistics per sample
+# 2) or collect statistics for a full corpus
+
+# Two extra options:
+# 1) Include gender information
+# 2) Include alignments
