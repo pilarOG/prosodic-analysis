be1c9b4084fa6b5ce1a7f80febbcce95ae62a6bc
pilarOG <psoplust@uc.cl>
2019-05-07 16:38:22 +0100

    fundamental freq analysis

--- analyse.py
+++ analyse.py
@@ -1,40 +1,41 @@
 from argparse import ArgumentParser
 from configuration import load_config
 from data_load import load_wave
-from freq_utils import extract_pitch
+from freq_utils import extract_pitch, draw_pitch
 import numpy as np
 #np.set_printoptions(threshold=np.inf)
 
 class ProsodicAnalysis():
-    def __init__(self, waveform):
+    def __init__(self, waveform, settings):
         self.wav = waveform
+        self.settings = settings
 
+    # 1) F0: mean, range, standard deviation, contour, un/voiced frames
     def set_pitch_analysis(self, pitch_analysis):
-
+        # Original Praat object, we will only take what we want
         self.pitch_analysis = pitch_analysis
-
         # Array with fundamental frequency values
         self.pitch_countour = pitch_analysis.selected_array['frequency']
         self.pitch_countour[self.pitch_countour==0] = np.nan
-
-        # Maximum value used to extract pitch values
-        self.ceiling = pitch_analysis.ceiling
-
-
-        self.mean_absolute_slope = pitch_analysis.get_mean_absolute_slope()
-
+        # Get plot of the contour
+        draw_pitch(self.pitch_countour, pitch_analysis.xs(), settings, 'original')
+        # Interpolated contour
+        self.interpolated = pitch_analysis.interpolate().selected_array['frequency']
+        self.interpolated[self.interpolated==0] = np.nan
+        draw_pitch(self.interpolated, pitch_analysis.xs(), settings, 'interpolated')
+        # Pitch slope
+        self.slope_pitch = pitch_analysis.get_mean_absolute_slope()
+        # General stats
+        self.mean_pitch = np.nanmean(self.pitch_countour)
         self.max_pitch = pitch_analysis.xmax
-
         self.min_pitch = pitch_analysis.xmin
-
         self.range = pitch_analysis.xrange
+        self.std_pitch = np.nanstd(self.pitch_countour)
+        # Percentage of voiced frames
+        self.perc_voiced = (pitch_analysis.count_voiced_frames() * 100) / pitch_analysis.get_number_of_frames()
 
-
-    #def get_pitch_analysis(self):
-    #    return self.pitch_analysis
 
 # Features to extract
-# 1) F0: mean, range, standard deviation, contour, un/voiced frames
 # 2) Intensity: mean, range, standard deviation, contour, mean c0 coefficient
 # 3) Duration: range, mean (without silences), standard deviation
 # 4) Speech rate: units per second
@@ -60,9 +61,9 @@ settings = load_config(opts.config)
 
 if settings.stats_sample:
     wav = load_wave(settings.stats_sample)
-    analysis = ProsodicAnalysis(wav)
+    analysis = ProsodicAnalysis(wav, settings)
 
     if settings.analyse_f0:
-        analysis.set_pitch_analysis(extract_pitch(analysis.wav))
+        analysis.set_pitch_analysis(extract_pitch(analysis.wav, settings))
 
-    print analysis.ceiling
+    print analysis.interpolated
--- freq_utils.py
+++ freq_utils.py
@@ -1,6 +1,15 @@
 # Functions for fundamental frequency analysis
-
+import numpy as np
 import parselmouth
+import matplotlib.pyplot as plt
 
-def extract_pitch(waveform):
-    return waveform.to_pitch()
+def extract_pitch(waveform, settings):
+    return waveform.to_pitch(pitch_ceiling=settings.pitch_ceiling, pitch_floor=settings.pitch_floor)
+
+# From original https://github.com/YannickJadoul/Parselmouth
+def draw_pitch(pitch_values, xaxis, settings, mode):
+    plt.clf()
+    plt.plot(xaxis, pitch_values)
+    plt.ylim(0, settings.pitch_ceiling)
+    plt.ylabel("fundamental frequency [Hz]")
+    plt.savefig(settings.save_plots+'/'+mode+'_'+settings.stats_sample.replace('wav', 'png'))
